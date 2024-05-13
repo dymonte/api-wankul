67f3f9bea0c8d88e9c2e25f95e3656eb6066ee28
Dylan <dylan.monteneri@gmail.com>
2024-05-13 15:18:15 +0200

    export de la fonction start

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
+
+
+const start = (port) => {
+    const app = express();
 
 
 
-const app = express();
-const port = 3000;
+    app.use(bodyParser.json());
 
-app.use(bodyParser.json());
+    const cardsJsonPath = path.resolve(__dirname, 'data/cards.json');
+    const cardsJson = fs.readFileSync(cardsJsonPath);
+    const cards = JSON.parse(cardsJson);
 
-const cardsJson = fs.readFileSync('data/cards.json');
-const cards = JSON.parse(cardsJson);
+    let commands = [
+        {
+            id: 1,
+            name: 'card',
+            description: 'List of cards'
+        }
+    ];
 
+    app.get('/v1', (req, res) => {
+        const commandsRef = commands.map(com => Object.assign({}, com, { "link": `/v1/${com.name}` }));
+        res.json(commandsRef);
+    });
 
-let commands=[
-    {
-        id : 1,
-        name: 'card',
-        description: 'List of cards'
-    }
-];
+    app.get('/v1/card', (req, res) => {
+        res.json(cards.map(card => { return { "id": card.id, "name": card.name, "link": `/v1/card/${card.id}` } }));
+    })
 
-app.get('/v1', (req, res) => {
-    const commandsRef = commands.map(com=>Object.assign({},com,{"link":`/v1/${com.name}`}));
-    res.json(commandsRef);
-});
+    app.get('/v1/card/:id', (req, res) => {
+        const id = req.params.id - 1;
+        if (!cards[id]) { return res.status(404).json({ error: 'Card not found' }); }
+        else {
+            const card = cards[id];
+            res.json(card);
+        }
 
-app.get('/v1/card', (req, res) => {
-    res.json(cards.map(card=>{return {"id" : card.id, "name" : card.name, "link" : `/v1/card/${card.id}`}}));
-})
+    })
+    app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-app.get('/v1/card/:id', (req, res) => {
-    const id = req.params.id-1;
-    if(!cards[id]) 
-        {return res.status(404).json({error: 'Card not found'});}
-    else{
-        const card = cards[id];
-        res.json(card);
-    }
+    return app;
+}
 
-})
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+module.exports = { start: start }
